7b25322e4a1b7bf604ea8d98e72bdc0a62068aa6
JohnFodero <jtfodero@gmail.com>
2020-07-12 12:20:05 -0400

    Added lane change logic and spline generation, passes driving rubric requirements

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
 using std::string;
 using std::vector;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
 
 int main() {
   uWS::Hub h;
@@ -49,9 +52,12 @@ int main() {
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
   }
+  int lane = 1;
+  double ref_vel = 0.0; //MPH
+  int state = 0; // 0 = kl, 1 = change lane;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+               &map_waypoints_dx,&map_waypoints_dy,&ref_vel,&lane, &state]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -63,12 +69,12 @@ int main() {
 
       if (s != "") {
         auto j = json::parse(s);
-        
+
         string event = j[0].get<string>();
-        
+
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
+
           // Main car's localization Data
           double car_x = j[1]["x"];
           double car_y = j[1]["y"];
@@ -80,23 +86,150 @@ int main() {
           // Previous path data given to the Planner
           auto previous_path_x = j[1]["previous_path_x"];
           auto previous_path_y = j[1]["previous_path_y"];
-          // Previous path's end s and d values 
+          // Previous path's end s and d values
           double end_path_s = j[1]["end_path_s"];
           double end_path_d = j[1]["end_path_d"];
 
-          // Sensor Fusion Data, a list of all other cars on the same side 
+          // Sensor Fusion Data, a list of all other cars on the same side
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
           json msgJson;
 
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
-
+          int prev_size = previous_path_x.size();
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+
+           /*
+           If not at max car_speed - check available lanes
+              If no cars within window of ego (aka if there is room ahead)
+                  State = change lane left
+           If change lane left, set lane to boundary
+           Change to Keep lane state if we have made it to the lane. This prevents the vehicle from changing back to the same lane
+
+           */
+          if(prev_size > 0) {
+            car_s = end_path_s;
+          }
+          bool too_close = false;
+          vector <int> open_lanes(3, 1);
+          for(int i = 0; i < sensor_fusion.size(); ++i) {
+            float d = sensor_fusion[i][6];
+            int actor_lane_idx = std::floor(d/4.0);
+            double vx = sensor_fusion[i][3];
+            double vy = sensor_fusion[i][4];
+            double check_speed = sqrt(vx*vx + vy*vy);
+            double check_car_s = sensor_fusion[i][5];
+            check_car_s += ((double)prev_size*0.02*check_speed);
+            if(d < (2+4*lane+2) && d > (2+4*lane-2)) {
+              if((check_car_s > car_s) && ((check_car_s-car_s) < 30)) {
+                //std::cout << "Too close!" << std::endl;
+                too_close = true;
+              }
+            }
+            if((check_car_s < car_s + 30) && (check_car_s > car_s - 10)) {
+              open_lanes[actor_lane_idx] = 0; // 0 means the lane is blocked
+            }
+          }
+          if(too_close) {
+            ref_vel -= 0.224;
+          }
+          else if(ref_vel < 49.5) {
+            ref_vel += 0.224;
+          }
+          if((ref_vel < 49.5) && (state == 0) && (j[1]["s"] > 160)) {
+            // change lanes
+            // if the lane is open
+            for(int i = 0; i < open_lanes.size(); ++i) {
+              if(i != lane && open_lanes[i] == 1 && std::abs(i-lane) == 1) {
+                lane = i;
+                state = 1;
+                std::cout << "CHANGING LANES TO: " << i << std::endl;
+                break;
+              }
+            }
+
+          }
+          if((state == 1) && car_d < (2+4*lane+2)-1 && car_d > (2+4*lane-2)+1 && end_path_d < (2+4*lane+2) && end_path_d > (2+4*lane-2)) {
+            // lane change complete, ensure it is fully in the lane and the trajectory has straightened before attempting another lane change
+            state = 0;
+          }
+          vector<double> ptsx;
+          vector<double> ptsy;
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+          if(prev_size < 2) {
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }
+          else {
+            ref_x = previous_path_x[prev_size-1];
+            ref_y = previous_path_y[prev_size-1];
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2];
+            ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+
+          vector<double> next_wp0 = getXY(car_s+30, 2+4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60, 2+4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s+90, 2+4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          for(int i = 0; i < ptsx.size(); ++i) {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+            ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y*sin(0-ref_yaw));
+            ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y*cos(0-ref_yaw));
+          }
+
+          tk::spline s;
+          s.set_points(ptsx, ptsy);
+
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+          for(int i = 0; i < previous_path_x.size(); ++i) {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+          double target_x = 30;
+          double target_y = s(target_x);
+          double target_dist = sqrt((target_x)*(target_x)+(target_y)*(target_y));
+          double x_add_on = 0;
+
+          for(int i = 1; i <= 50-previous_path_x.size(); ++i) {
+            double N = (target_dist/(0.02*ref_vel/2.24));
+            double x_point = x_add_on+(target_x)/N;
+            double y_point = s(x_point);
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            x_point = (x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
+            y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw));
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
 
 
           msgJson["next_x"] = next_x_vals;
@@ -131,6 +264,6 @@ int main() {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
